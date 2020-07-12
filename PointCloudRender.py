3508fe7b6959364d40ff94cadb6b0b7470c9e7dd
lhubbard@pdx.edu <lhubbard@pdx.edu>
2020-07-11 19:37:49 -0700

    Prettied up the PointCloudRender, added necessary import statements

--- PointCloudRender.py
+++ PointCloudRender.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+
+
+
 class CloudRender:
   def __init__(self, path):
     self.path = path
@@ -11,9 +16,21 @@ class CloudRender:
   def load_cloud(self):
     with open(self.path, "r") as f:
       x=f.readlines()
+    
     counts = list(map(int, x[1].strip().split(" ")))
-    points = np.array([list(map(float, x[i].strip().split(" "))) for i in range(2,counts[0]+2)])
-    indices = [list(map(int, x[i].strip().split(" ")[1:])) for i in range(2+counts[0], counts[1]+2+counts[0])]
+    
+
+
+    points = np.array(
+                [ list(map(float, x[i].strip().split(" "))) 
+                    for i in range(2, counts[0] + 2)]
+                )
+    
+
+
+    indices = [ list( map( int, x[i].strip().split(" ")[1:])) 
+                    for i in range(2 + counts[0], counts[1] + 2 + counts[0])]
+
     return points, indices, map_vertices(points, indices)
 
 
@@ -28,22 +45,34 @@ class CloudRender:
 
 
   def show_cloud(self):
-    fig, ax = plt.subplots(figsize=((15,9)),
-                          subplot_kw={"projection":"3d", "elev":0})
-    ax.scatter(self.points[:,0], self.points[:,1], self.points[:,2])
+    fig, ax = plt.subplots(figsize=((15, 9)),
+                          subplot_kw={"projection" : "3d", "elev" : 0})
+    ax.scatter(self.points[:, 0], self.points[:, 1], self.points[:, 2])
     return fig
 
-  def show_all(self,elev=0.0,azimuth=0.0):
+  def show_all(self, elev=0.0, azimuth=0.0):
     if self.tris is None:
-      self.tris = mtri.Triangulation(x=self.points[:,0],y=self.points[:,1],triangles=self.indices)
-    fig, (ax1,ax2) = plt.subplots(ncols=2, figsize=((15,15)), subplot_kw={"projection":"3d", "elev":elev,"azim":azimuth})
-    ax1.plot_trisurf(self.tris,self.points[:,2])
-    ax2.scatter(self.points[:,0], self.points[:,1], self.points[:,2])
+      self.tris = mtri.Triangulation(x = self.points[:, 0],
+                                     y = self.points[:, 1],
+                                     triangles = self.indices)
+
+    fig, (ax1,ax2) = plt.subplots(ncols = 2, figsize = ((15, 15)), 
+                                  subplot_kw={"projection" : "3d", 
+                                              "elev" : elev,
+                                              "azim":azimuth}
+                                 )
+
+    ax1.plot_trisurf(self.tris, self.points[:, 2])
+    ax2.scatter(self.points[:,0], self.points[:, 1], self.points[:, 2])
+
     return fig
 
   def show_verts(self,):
-    fig, ax = plt.subplots(figsize=((15,9)),subplot_kw={"projection":"3d", "elev":0})
+    fig, ax = plt.subplots(figsize=((15, 9)),
+                           subplot_kw={"projection" : "3d", "elev" : 0})
     if self.tris is None:
-      self.tris = mtri.Triangulation(x=self.points[:,0],y=self.points[:,1],triangles=self.indices)
-    ax.plot_trisurf(self.tris,self.points[:,2])
+      self.tris = mtri.Triangulation(x = self.points[:,0],
+                                     y = self.points[:,1],
+                                     triangles = self.indices)
+    ax.plot_trisurf(self.tris, self.points[:, 2])
     return fig
